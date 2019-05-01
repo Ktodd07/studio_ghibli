0b6e8d5ab2db16bd5587a25455bd56179a06abe0
Diapause <diapause@protonmail.com>
2019-05-01 13:33:10 -0700

    first pass at CLI

---
+++ lib/studio_ghibli.rb
@@ -0,0 +1,2 @@
+require_relative "./studio_ghibli/version"
+require_relative "./studio_ghibli/cli"
---
+++ lib/studio_ghibli/cli.rb
@@ -0,0 +1,89 @@
+#CLI controller
+
+class String
+
+    def red; colorize(self, "\e[1m\e[31m"); end
+    def green; colorize(self, "\e[1m\e[32m"); end
+    def dark_green; colorize(self, "\e[32m"); end
+    def yellow; colorize(self, "\e[1m\e[33m"); end
+    def blue; colorize(self, "\e[1m\e[34m"); end
+    def dark_blue; colorize(self, "\e[34m"); end
+    def pur; colorize(self, "\e[1m\e[35m"); end
+    def colorize(text, color_code)  "#{color_code}#{text}\e[0m" end
+end
+
+class StudioGhibli::CLI
+
+  def call
+    sweet_ascii_greeting
+    main_menu
+    user_input = gets.strip.downcase
+    validate(user_input)
+  end
+
+  def sweet_ascii_greeting
+    puts " "
+    puts "           WELCOME TO STUDIO GHIBLI!".red
+    puts " "
+    puts <<-DOC.gsub(/^\s+/, "")
+    dNNNNNNNNNNNNNNNNNb  oo  ooo o  o ooo.  o .ooo.
+    NNNNNN'`NNN'`NNNNNN 8  "  8  8  8 8  `b 8 8   8
+    NNNNNF  YNP  YNNNNN "oo.  8  8  8 8   8 8 8   8
+    NNNNN'  `V'  `NNNNN    8  8  8  8 8   8 8 8   8
+    NNNNF _     _ YNNNN o  8  8  8  8 8  ,P 8 8   8
+    NNNN (.)   (.) NNNN  ""   "   ""  """'  "  """
+    NNNF  "     "  YNNN   ooo.  o   o o ooo   o   o
+    NNN'           `NNN .P   "  8   8 8 8  8  8   8
+    NNN             NNN 8       8ooo8 8 8oob  8   8
+    NNN             NNM 8   ""8 8   8 8 8  `b 8   8
+    N""e           s""M "b   d" 8   8 8 8  .8 8   8
+    YCGMMNNNNNNNNNMMGCP   """   "   " " """"  """ "
+
+    DOC
+    puts " "
+  end
+
+  def main_menu
+    puts " "
+    puts "                  MAIN MENU".blue
+    puts " "
+    puts "About".yellow + " - About Studio Ghibli"
+    puts "Films".yellow + " - Studio Ghibli Film Collection"
+    puts "Exit".yellow
+    puts " "
+  end
+
+  def validate(user_input)
+    until user_input == "about" || user_input == "films" || user_input == "exit"
+      puts " "
+      puts "Invalid Entry".red
+      puts " "
+      puts "Please select from the menu items in yellow"
+      puts " "
+      user_input = gets.strip.downcase
+    end
+
+    if user_input == "about"
+      puts " "
+      puts "             ABOUT STUDIO GHIBLI".blue
+      puts " "
+    elsif user_input == "films"
+      puts " "
+      puts "        STUDIO GHIBLI FILM COLLECTION".blue
+      puts " "
+    elsif user_input == "exit"
+      puts " "
+      puts <<-DOC.red
+       _____
+      /     \\              SAYONARA!
+      vvvvvvv  /|__/|
+         I   /O,O   |
+         I /_____   |      /|/|
+         J/^ ^ ^ \\  |    /00  |    _//|
+         |^ ^ ^ ^ |W|   |/^^\\ |  /oo  |
+          \\m___m__|_|    \\m_m_|  \\mm_ |
+
+      DOC
+    end
+  end
+end
